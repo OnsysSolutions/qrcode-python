fbd69dff5e90849f734c08cd2833c45a5c609b0f
Adeir Junior <hello@adeir.me>
2025-03-12 09:35:47 -0400

    render

--- render-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Atualiza os pacotes do sistema
-apt-get update && apt-get install -y libzbar0
--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port 10000
+#!/usr/bin/env bash
+
+# Instala libzbar0 no ambiente de execução
+sudo apt-get update && sudo apt-get install -y libzbar0
+
+# Inicia a aplicação
+uvicorn main:app --host 0.0.0.0 --port 8000
